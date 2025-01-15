3e10f2ea4b2480111cc0d21d07307184ca3c6572
Padmavathi <111369041+Padmavathi06@users.noreply.github.com>
2025-01-15 19:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,35 @@ import json
 import pickle
 import numpy as np
 import pandas as pd
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
-from typing import List, Dict
-import uvicorn
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
 
 # Load the model
 regmodel = pickle.load(open('air_quality.pkl', 'rb'))
 scalar = pickle.load(open('scaler.pkl', 'rb'))
 
-app = FastAPI()
+@app.route('/')
+def home():
+    return render_template('frontend.html')
 
-class DataModel(BaseModel):
-    data: Dict[str, float]
-
-@app.get("/")
-async def home():
-    return {"message": "Welcome to the Air Quality Prediction API"}
-
-@app.post("/predict_api")
-async def predict_api(data: DataModel):
-    new_data = scalar.transform(np.array(list(data.data.values())).reshape(1, -1))
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
-    return {"prediction": output[0]}
+    print(output[0])
+    return jsonify(output[0])
 
-@app.post("/predict")
-async def predict(data: List[float]):
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
     final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
     output = regmodel.predict(final_input)[0]
-    return {"prediction": f"Air Quality {output}"}
+    return render_template("frontend.html", prediction_text="Air Quality {}".format(output))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    app.run(debug=True)
