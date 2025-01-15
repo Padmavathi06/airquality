02c72c33d74bcc4803d70ce3b65379f0a2185111
Padmavathi <111369041+Padmavathi06@users.noreply.github.com>
2025-01-15 19:06:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,37 @@ import json
 import pickle
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template, request, jsonify
+import gradio as gr
 
-app = Flask(__name__)
-
-# Load the model
+## Load the model
 regmodel = pickle.load(open('air_quality.pkl', 'rb'))
 scalar = pickle.load(open('scaler.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('frontend.html')
-
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
+def predict_api(data):
     new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    return output[0]
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = [float(x) for x in request.form.values()]
+def predict(data):
+    data = [float(x) for x in data]
     final_input = scalar.transform(np.array(data).reshape(1, -1))
-    print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template("frontend.html", prediction_text="Air Quality {}".format(output))
+    return f"Air Quality {output}"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            input_data = gr.Textbox(label="Input Data (JSON format)")
+            predict_button = gr.Button("Predict")
+            output_text = gr.Textbox(label="Prediction Output")
+
+        predict_button.click(fn=predict_api, inputs=input_data, outputs=output_text)
+
+    with gr.Row():
+        input_data_form = gr.Textbox(label="Input Data (Form format)")
+        predict_button_form = gr.Button("Predict")
+        output_text_form = gr.Textbox(label="Prediction Output")
+
+        predict_button_form.click(fn=predict, inputs=input_data_form, outputs=output_text_form)
+
+demo.launch(debug=True)
